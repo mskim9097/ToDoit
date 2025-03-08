619db56c0f5cac2da229a6b45b27025b787bcce0
mskim9097 <mskim9097@gmail.com>
2025-03-07 22:30:58 -0800

    create reminder, task html and connect footer, header

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,26 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
-      }).catch((error) => {
+    }).catch((error) => {
         // An error happened.
-      });
+    });
 }
+
+function logo() {
+    firebase.auth().onAuthStateChanged(user => {
+        var logo = document.querySelector('a.navbar-brand');
+    
+        if (user) {
+            console.log("login");
+            logo.href = "/main";
+        } else {
+            console.log("not login");
+            logo.href = "/";
+        }
+    });
+}
+
+logo();
+
+
+
