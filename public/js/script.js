272c8c635f92710c78dc26ae0acd870fa1a2e4f8
mskim9097 <mskim9097@gmail.com>
2025-03-07 22:54:10 -0800

    logo href auto change when signin and signout

--- public/js/script.js
+++ public/js/script.js
@@ -7,21 +7,7 @@ function logout() {
     });
 }
 
-function logo() {
-    firebase.auth().onAuthStateChanged(user => {
-        var logo = document.querySelector('a.navbar-brand');
-    
-        if (user) {
-            console.log("login");
-            logo.href = "/main";
-        } else {
-            console.log("not login");
-            logo.href = "/";
-        }
-    });
-}
 
-logo();
 
 
 
