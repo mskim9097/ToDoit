40e0cd387a3e9cf6f6a9a2d32f4dd3174b7ebcfc
Aarushi Sharma <aarushisharma1110@gmail.com>
2025-03-18 14:34:10 -0700

    added create button

--- public/js/create.js
+++ public/js/create.js
@@ -1,19 +1,28 @@
-document.querySelector(".reminder-btn").addEventListener("click", function() {
-    var reminderRef = db.collection("reminder");
-    var auth = firebase.auth();
+document.querySelector("createGroupButton").addEventListener("click", function () {
+  var groupRef = db.collection("groups");
+  var auth = firebase.auth();
 
-    auth.onAuthStateChanged((user) => {
-        if(user) {
-            reminderRef.add({
-                reminder_title: document.getElementById("reminder-title").value,
-                reminder_create_date: firebase.firestore.FieldValue.serverTimestamp(),
-                reminder_delete_fg: "N",
-                user_no: user.uid
-            }).then((docRef) => {
-                window.location.href = "/reminder?docID=" + docRef.id; 
-            }).catch ((error) => {
-            console.error("failed creating reminder");
-            });
-        }
-    });
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      groupRef
+        .add({
+          group_name: document.getElementById("groupName").value,
+          group_cover: document.getElementById("groupCover").files[0]
+            ? document.getElementById("groupCover").files[0].name
+            : "", // Handle file upload
+          group_privacy: document.getElementById("groupPrivacy").value,
+          group_roles: document.getElementById("groupRoles").value,
+          group_color: document.getElementById("groupColor").value,
+          group_description: document.getElementById("groupDescription").value,
+          group_create_date: firebase.firestore.FieldValue.serverTimestamp(), // Create timestamp
+          user_no: user.uid, // Use the authenticated user's ID
+        })
+        .then((docRef) => {
+          window.location.href = "/group?docID=" + docRef.id;
+        })
+        .catch((error) => {
+          console.error("failed creating reminder");
+        });
+    }
+  });
 });
