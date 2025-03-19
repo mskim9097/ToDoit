2e704e9a6055d94d9e731d60e50c26556767cc9b
Aarushi Sharma <aarushisharma1110@gmail.com>
2025-03-19 12:28:19 -0700

    corrected the navbar

--- public/js/create.js
+++ public/js/create.js
@@ -1,19 +1,14 @@
-document.querySelector("createGroupButton").addEventListener("click", function () {
-  var groupRef = db.collection("groups");
+document.querySelector("#createGroupButton").addEventListener("click", function () {
+  var groupRef = db.collection("Group");
   var auth = firebase.auth();
-
+  console.log("test");
   auth.onAuthStateChanged((user) => {
     if (user) {
+      console.log(document.getElementById("groupName").value);
       groupRef
         .add({
           group_name: document.getElementById("groupName").value,
-          group_cover: document.getElementById("groupCover").files[0]
-            ? document.getElementById("groupCover").files[0].name
-            : "", // Handle file upload
           group_privacy: document.getElementById("groupPrivacy").value,
-          group_roles: document.getElementById("groupRoles").value,
-          group_color: document.getElementById("groupColor").value,
-          group_description: document.getElementById("groupDescription").value,
           group_create_date: firebase.firestore.FieldValue.serverTimestamp(), // Create timestamp
           user_no: user.uid, // Use the authenticated user's ID
         })
