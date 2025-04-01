dce9ab50bf016a81e92f80a781ae050feef3db87
mskim9097 <mskim9097@gmail.com>
2025-03-31 21:09:11 -0700

    notification badge

--- public/js/header.js
+++ public/js/header.js
@@ -1,15 +1,55 @@
-function logo() {
-    firebase.auth().onAuthStateChanged(user => {
+firebase.auth().onAuthStateChanged(user => {
+    function logo() {
         var logo = document.querySelector('a.navbar-brand');
-
+        
         if (user) {
-            console.log("login");
             logo.href = "/main";
         } else {
-            console.log("not login");
             logo.href = "/";
         }
-    });
-}
+        
+    }
+    logo();
+    
+    function selectBadgeCount() {
+        let urgent = 0;
+        const today = new Date();
+        
+        db.collection("Group")
+        .where("members", "array-contains", user.uid)
+        .get()
+        .then(groupSnapshot => {
+            groupSnapshot.forEach(groupDoc => {
+                db.collection("Group")
+                .doc(groupDoc.id)
+                .collection("task")
+                .orderBy("dueDate", "asc")
+                .onSnapshot(taskSnapshot => {
+                    taskSnapshot.forEach(taskDoc => {
+                        let dueDateStr = taskDoc.data().dueDate;
+                        let dueDate = new Date(dueDateStr + "T00:00:00");
+                        let diffInTime = dueDate - today;
+                        let diffInDays = Math.ceil(diffInTime / (1000 * 3600 * 24));
+                        if (diffInDays >= 0 && diffInDays <= 2) {
+                            urgent++;                        
+                        }
+                    });
+                    if(urgent == 0) {
+                        document.querySelectorAll(".badge").forEach(doc => {
+                            doc.style.visibility = "hidden";
+                        })
+                    } else {
+                        
+                        document.querySelectorAll(".badge").forEach(doc => {
+                            doc.style.visibility = "visible";
+                            doc.innerHTML = "";
+                            doc.innerHTML = urgent;
+                        })
+                    }
+                });
+            });
+        });
+    }
+    selectBadgeCount();
 
-logo();
+});
