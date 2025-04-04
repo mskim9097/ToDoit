0f1e3ca7fb0f0a749adb1c1bf04cf0bc8b1cb828
Shaz Uqaili <suqaili99@hotmail.com>
2025-04-04 12:42:13 -0700

    Added update function, current implementation is buggy, made formatting changes to task.js

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,8 @@ function doAll() {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             currentUser = db.collection("user").doc(user.uid); //global
+            console.log(currentUser);
+
             // figure out what day of the week it is today
             const weekday = [
                 "sunday",
@@ -40,6 +42,8 @@ function readQuote(day) {
     db.collection("quote")
         .doc(day)
         .onSnapshot((doc) => {
+            console.log("inside");
+            console.log(doc.data());
             document.getElementById("quote-goes-here").innerHTML = doc.data().quote;
         });
 }
@@ -67,6 +71,8 @@ function getNameFromAuth() {
         // Check if a user is signed in:
         if (user) {
             // Do something for the currently logged-in user here:
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName); //print the user name in the browser console
             userName = user.displayName;
 
             //method #1:  insert with JS
