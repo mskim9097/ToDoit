ed7672146d7d30c7739e5c88f9a5ff291503d990
mskim9097 <mskim9097@gmail.com>
2025-04-09 15:25:21 -0700

    real final version removed unused codes

--- public/js/invite.js
+++ public/js/invite.js
@@ -38,34 +38,3 @@ function addGroupMember() {
         });
 };
 addGroupMember();
-/*
-    var reminderMemberRef = db.collection("reminder_member");
-    reminderMemberRef.add({
-        reminder_manager: "N",
-        reminder_member_create_date: firebase.firestore.FieldValue.serverTimestamp(),
-        reminder_delete_fg: "N",
-        reminder_no: groupID,
-
-
-    })
-  */
-/*
-document.querySelector(".reminder-btn").addEventListener("click", function() {
-    var reminderRef = db.collection("reminder");
-    var auth = firebase.auth();
-
-    auth.onAuthStateChanged((user) => {
-        if(user) {
-            reminderRef.add({
-                reminder_title: document.getElementById("reminder-title").value,
-                reminder_create_date: firebase.firestore.FieldValue.serverTimestamp(),
-                reminder_delete_fg: "N",
-                user_no: user.uid
-            }).then((docRef) => {
-                window.location.href = "/reminder?docID=" + docRef.id; 
-            }).catch ((error) => {
-            console.error("failed creating reminder");
-            });
-        }
-    });
-});*/
